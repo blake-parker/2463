42938eb82199911d438aefc3e0acd0e4d293d628
Blake <bparker9902@gmail.com>
2024-01-18 10:49:33 -0600

    added documentation

--- Graphics/Assignment1/sketch1.js
+++ Graphics/Assignment1/sketch1.js
@@ -3,6 +3,7 @@ function setup() {
 }
 
 function draw() {
+  //its a circle and a rectangle... i hope i dont need to add documentation
   background(133, 250, 90);
   ellipse(150, 150, 240, 240);
   rect(330, 30, 240, 240);
--- Graphics/Assignment1/sketch2.js
+++ Graphics/Assignment1/sketch2.js
@@ -4,11 +4,17 @@ function setup() {
 
 function draw() {
   background("white");
-  noStroke();
+  noStroke(); //turning off outlines for shapes
+
+  //red circle
   fill(255, 0, 0, 90);
   ellipse(150, 110, 110, 110);
+
+  //blue circle
   fill(0, 0, 255, 90);
   ellipse(110, 170, 110, 110);
+
+  //green circle
   fill(0, 255, 0, 90);
   ellipse(190, 170, 110, 110);
 }
