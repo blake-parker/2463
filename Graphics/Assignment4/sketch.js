dff869f7f9bf8ffff60bfab5a2f636b1252f2989
Blake Parker <bparker9902@gmail.com>
2024-02-14 17:33:44 -0600

    bug squish done

--- Graphics/Assignment4/sketch.js
+++ Graphics/Assignment4/sketch.js
@@ -1,35 +1,184 @@
 let score;
 let timer;
+let state = 0;
+let speed = 1;
+let sheet;
+let bugs = [];
+
+let test;
+
+function preload() {
+  sheet = loadImage("Roach.png");
+}
 
 function setup() {
   createCanvas(800, 500);
   imageMode(CENTER);
+  angleMode(DEGREES);
   timer = new Timer("Time remaining:", 2, 25);
-  score = new Score(700, 25);
+  score = new Score(650, 25);
   textSize(25);
+  for (let i = 0; i <= 10; i++) {
+    let r1 = random(0, 100);
+    let r2, r3;
+    if (r1 <= 50) {
+      r2 = random(0, -100);
+      r3 = 1;
+    } else {
+      r2 = random(800, 900);
+      r3 = -1;
+    }
+    bugs[i] = new Bug(sheet, 40, 40, r2, random(26, 490), 6, r3);
+  }
 }
 
 function draw() {
   background("white");
-  timer.draw();
-  timer.start();
-  score.draw();
+  if (state === 0) {
+    text("Bug Squish Game!", 325, 250);
+    text('Press "Space" to start!', 300, 300);
+  }
+  if (state === 1) {
+    timer.draw();
+    timer.start();
+    score.draw();
+    bugs.forEach((e) => {
+      e.draw();
+    });
+    if (timer.getTimer() <= 0) {
+      timer.stop();
+      state += 1;
+    }
+  }
+  if (state === 2) {
+    text("Game Over!", 350, 250);
+    text(`Your score was ${score.getScore()}`, 325, 300);
+  }
+}
+
+function keyPressed() {
+  if (keyCode === 32 && state === 0) {
+    state += 1;
+  }
+}
+
+function mousePressed() {
+  bugs.forEach((e) => {
+    e.mousePressed(mouseX, mouseY);
+  });
 }
 
 class Bug {
-  constructor(image, x, y, speed) {
-    this.image = image;
-    this.x = x;
-    this.y = y;
-    this.speed = speed;
+  constructor(spriteSheet, sw, sh, dx, dy, aLen, move) {
+    this.spriteSheet = spriteSheet;
+    this.sw = sw;
+    this.sh = sh;
+    this.dx = dx;
+    this.dy = dy;
+    this.u = 0;
+    this.v = 0;
+    this.aLen = aLen;
+    this.curFrame = 0;
+    this.move = move;
+    this.xDir = 1;
+    this.dead = false;
+    this.counter = 0;
   }
 
   draw() {
-    image(this.image, this.x, this.y);
+    if (this.move != 0) {
+      this.u = this.curFrame % this.aLen;
+    } else {
+      this.u = 0;
+    }
+
+    push();
+    translate(this.dx, this.dy);
+    scale(this.xDir, 1);
+    if (this.move <= -1) rotate(-90);
+    if (this.move >= 1) rotate(90);
+    if (this.dead === true) {
+      if (frameCount % 60 === 0) {
+        this.counter++;
+      }
+      if (this.counter >= 3) {
+        image(
+          this.spriteSheet,
+          0,
+          0,
+          this.sw,
+          this.sh,
+          280,
+          0,
+          this.sw,
+          this.sh
+        );
+      } else {
+        image(
+          this.spriteSheet,
+          0,
+          0,
+          this.sw,
+          this.sh,
+          240,
+          0,
+          this.sw,
+          this.sh
+        );
+      }
+    } else {
+      image(
+        this.spriteSheet,
+        0,
+        0,
+        this.sw,
+        this.sh,
+        this.u * this.sw,
+        this.v * this.sh,
+        this.sw,
+        this.sh
+      );
+    }
+
+    pop();
+    if (frameCount % 6 === 0) {
+      this.curFrame++;
+    }
+    this.dx += this.move * speed;
+    if (this.dx > 900) {
+      this.dx = 0;
+    } else if (this.dx < -100) {
+      this.dx = 800;
+    }
   }
 
   stop() {
     this.speed = 0;
+  }
+
+  mousePressed(x, y) {
+    if (
+      this.dx - 30 < x &&
+      x < this.dx + 30 &&
+      this.dy - 30 < y &&
+      y < this.dy + 30 &&
+      !this.dead
+    ) {
+      this.move = 0;
+      this.dead = true;
+      score.incScore(1);
+      speed++;
+      let r1 = random(0, 100);
+      let r2, r3;
+      if (r1 <= 50) {
+        r2 = random(0, -100);
+        r3 = 1;
+      } else {
+        r2 = random(800, 900);
+        r3 = -1;
+      }
+      bugs.push(new Bug(sheet, 40, 40, r2, random(26, 490), 6, r3));
+    }
   }
 }
 
@@ -74,5 +223,9 @@ class Score {
 
   incScore(amt) {
     this.curScore += amt;
+  }
+
+  getScore() {
+    return this.curScore;
   }
 }
