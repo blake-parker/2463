ea5a2dfafe3d21b093fbe011f9fe6667805c9004
Blake Parker <bparker9902@gmail.com>
2024-02-28 19:02:18 -0600

    Assignment 2 Done

---
+++ Audio/Assignment2/sketch.js
@@ -0,0 +1,106 @@
+let synth = new Tone.PolySynth(Tone.Synth);
+let bend = new Tone.PitchShift();
+let dist = new Tone.Distortion(0.8);
+
+bend.pitch = 0;
+dist.distortion = 0;
+synth.connect(bend);
+bend.connect(dist);
+dist.toDestination();
+
+let sliderY = 200;
+function setup() {
+  let canv = createCanvas(900, 500);
+  let cp = canv.position();
+
+  let pSlider = createSlider(0, 12, 0, 0.1);
+  pSlider.style("transform", "rotate(270deg)");
+  pSlider.position(cp.x + 260, cp.y + sliderY);
+  pSlider.mouseMoved(() => {
+    bend.pitch = pSlider.value();
+  });
+
+  let dSlider = createSlider(0, 1, 0, 0.05);
+  dSlider.style("transform", "rotate(270deg)");
+  dSlider.position(cp.x + 360, cp.y + sliderY);
+  dSlider.mouseMoved(() => {
+    dist.distortion = dSlider.value();
+  });
+}
+let notes = {
+  a: "C4",
+  s: "D4",
+  d: "E4",
+  f: "F4",
+  g: "G4",
+  h: "A4",
+  j: "B4",
+  k: "C5",
+  l: "D5",
+};
+
+let kbKeys = ["Keys", "A", "S", "D", "F", "G", "H", "J", "K", "L"];
+let nList = ["Notes", "C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5"];
+function keyPressed() {
+  mostRecentKeyPressed = key;
+  console.log(mostRecentKeyPressed);
+  let playNotes = notes[key];
+  synth.triggerAttack(playNotes);
+}
+
+function keyReleased() {
+  let playNotes = notes[key];
+  synth.triggerRelease(playNotes, "+0.03");
+}
+
+let mostRecentKeyPressed = "";
+function draw() {
+  background(420, 0, 69);
+  createHeading("Assignment 2");
+
+  let i = 0;
+  let j = 0;
+  let fillColor = "";
+  kbKeys.forEach((e) => {
+    if (mostRecentKeyPressed.toUpperCase() === e) {
+      fillColor = 222;
+    }
+    r(j, fillColor, 400);
+    fillColor = "white";
+    t(e, j, 425);
+    j++;
+  });
+
+  nList.forEach((e) => {
+    r(i, fillColor, 350);
+    t(e, i, 375);
+    i++;
+  });
+  textAlign(CENTER, CENTER);
+  fill(255);
+  text("Pitch", 325, 300);
+  text("Distortion", 430, 300);
+}
+
+function createHeading(input) {
+  fill("white");
+  text(input, 450, 50);
+  text(
+    "Press a key labeled below. Use the sliders to adjust their effects!",
+    450,
+    80
+  );
+  textSize(20);
+  textAlign(CENTER);
+}
+
+function r(i, fc, y) {
+  fill(fc);
+  rect(200 + 50 * i, y, 50, 50);
+}
+
+function t(e, j, y) {
+  fill(0);
+  textAlign(CENTER, CENTER);
+  text(e, 225 + 50 * j, y);
+}
